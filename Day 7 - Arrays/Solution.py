e4b29a8d76b47eaaf74cf39a7494ab1e0c96d80e
herokudev <ao43085@gmail.com>
2021-09-24 12:18:26 -0600

    Code for challenge#8
    Code for challenge#8

---
+++ Day 7 - Arrays/Solution.py
@@ -0,0 +1,17 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    arr.reverse()
+
+    for num in arr:
+        print(str(num) + " ", end="")
+        
