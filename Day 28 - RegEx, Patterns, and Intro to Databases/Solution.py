938dd7c43702fcf046761c3f5679adbeb2d119c4
herokudev <ao43085@gmail.com>
2021-11-17 17:27:56 -0600

    Code for challenge#29
    Code for challenge#29

---
+++ Day 28 - RegEx, Patterns, and Intro to Databases/Solution.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    arr = []
+
+    n = int(input())
+
+    for i in range(n):
+        data = str(input()).split(" ")
+        name = data[0]
+        email = data[1]
+
+        if re.search(".+@gmail\.com$", email):
+            arr.append(name)
+
+    arr.sort()
+
+    for name in arr:
+        print(name)
